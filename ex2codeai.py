aee47ebfd0ed7fda13fc7b718e298eb3ce9925ac
M.Amin Tehrani <t.mohammadamin@gmail.com>
2024-11-21 15:39:16 +0100

    feat: imlp ModuleSpec

--- ex2codeai.py
+++ ex2codeai.py
@@ -1,7 +1,19 @@
 import keyword
 import copy
+import types
 from langchain.prompts import PromptTemplate
 from langchain.llms.base import LLM
+import dill
+
+
+def save_dill(path: str, obj):
+    with open(path, "wb") as f:
+        dill.dump(obj, f)
+
+def save_module(path: str, obj_str):
+    assert path.endswith(".py"), "Module path must be a .py file"
+    with open(path, "w") as f:
+        f.write(obj_str)
 
 
 class Example:
@@ -62,10 +74,16 @@ class Spec:
         """
         str_obj = str_obj.replace("```", "")
         exec(str_obj)
-        return eval(self.name)
+        obj = eval(self.name)
+        return self._wrap_generated_obj(obj, str_obj)
 
     def prompt(self):
         return self.PROMPT.format(**self.to_dict())
+        
+    def _wrap_generated_obj(self, obj, str_obj):
+        obj.__name__ = self.name
+        obj.__doc__ = self.desc
+        return obj
     
 
 class FuncSpec(Spec):
@@ -106,6 +124,10 @@ class FuncSpec(Spec):
     
     def __repr__(self):
         return f"Name: {self.name}, Description: {self.desc}, Examples: {self.examples}\n"
+    
+    def _wrap_generated_obj(self, obj, str_obj):
+        obj = super()._wrap_generated_obj(obj, str_obj)
+        return obj
     
 
 class ClassSpec(Spec):
@@ -181,3 +203,74 @@ class ClassSpec(Spec):
         d["class_methods"] = copy.deepcopy(self.class_methods)
         d["static_methods"] = copy.deepcopy(self.static_methods)
         return d
+
+    def __repr__(self):
+        return f"Class Name: {self.name}, Description: {self.desc}\nInstance Methods:{self.instance_methods}\nClass Methods:{self.class_methods}\nStatic Methods:{self.static_methods}\n"
+
+class ModuleSpec(Spec):
+    TEMPLATE = """
+            You are a Python programmer. Your task is to write a simple Python module that matches the following description, functions and classes.
+
+            Description:
+            {desc}
+
+            Functions:
+            {functions}
+
+            Classes:
+            {classes}
+
+            Write a Python module named `{name}` that implements this logic. Make sure you are importing the necessary modules. Do not include any additional explanation or comments, just the code.
+        """
+    PROMPT = PromptTemplate.from_template(TEMPLATE)
+
+    def __init__(self, name, desc):
+        super().__init__(name, desc)
+        self.classes = []
+        self.functions = []
+
+    def add_class(self, name:str, desc: str, *examples: Example):
+        """
+        Add a class to the module
+
+        Args:
+            name (str): Name of the class
+            desc (str): Description of the class
+            examples (Example): Examples of the class
+        """
+        cls = ClassSpec(name, desc, *examples)
+        self.classes.append(cls)
+        return cls
+    
+    def add_function(self, name:str, desc: str, *examples: Example):
+        """
+        Add a function to the module
+
+        Args:
+            name (str): Name of the function
+            desc (str): Description of the function
+            examples (Example): Examples of the function
+        """
+        f = FuncSpec(name, desc, *examples)
+        self.functions.append(f)
+        return f
+    
+
+    def to_dict(self):
+        d = super().to_dict()
+        d["functions"] = copy.deepcopy(self.functions)
+        d["classes"] = copy.deepcopy(self.classes)
+        return d
+    
+    def parse(self, str_obj: str):
+        """
+        Parse the output of the LLM
+
+        Args:
+            str_obj (str): Output of the LLM
+        """
+        str_obj = str_obj.replace("```", "")
+        module = types.ModuleType(self.name)
+        exec(str_obj, module.__dict__)
+        
+        return self._wrap_generated_obj(module, str_obj)
