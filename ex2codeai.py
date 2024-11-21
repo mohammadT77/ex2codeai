fc7f759c0d295a0b4aa4f1309397a6fdbc072a33
M.Amin Tehrani <t.mohammadamin@gmail.com>
2024-11-21 11:59:39 +0100

    refactor: rename files

--- ex2codeai.py
+++ ex2codeai.py
@@ -1,60 +1,134 @@
-from spec import *
-from langchain_ollama.llms import OllamaLLM
+import keyword
+import copy
+from langchain.prompts import PromptTemplate
+from langchain.llms.base import LLM
 
 
-llm = OllamaLLM(base_url="https://4e61-34-145-10-216.ngrok-free.app", model="llama3")
+class Example:
+    def __init__(self, input: dict, output):
+        self.input = input
+        self.output = output
+
+    def to_dict(self):
+        return {
+            "input": self.input,
+            "output": self.output
+        }
+
+    def __repr__(self):
+        inp = ",".join([f"{k}: {v}" for k, v in self.input.items()])
+        return f"Input: ({inp}), Output: {self.output}\n"
+
+class Spec:
+    PROMPT = None
+
+    def __init__(self, name: str, desc=""):
+        assert name.isidentifier() and not keyword.iskeyword(name), "Name must be a valid Python identifier"
+        self.name = name
+        self.desc = desc
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "desc": self.desc
+        }
+    
+    def invoke(self, llm: LLM):
+        chain = self.PROMPT | llm
+        return chain.invoke(self.to_dict())
+
+    def generate(self, llm: LLM):
+        return self.parse(self.invoke(llm))
+
+    def parse(self, str_obj: str):
+        str_obj = str_obj.replace("```", "")
+        exec(str_obj)
+        f = eval(self.name)
+        return f
+
+    def prompt(self):
+        return self.PROMPT.format(**self.to_dict())
+    
+
+class FuncSpec(Spec):
+    TEMPLATE = """
+            You are a Python programmer. Your task is to write a Python function that matches the following examples.
+            Each example consists of an input and its respective output.
+
+            Examples:
+            {examples}
+
+            Description:
+            {desc}
+
+            Write a Python function named `{name}` that implements this logic. Do not include any additional explanation or comments, just the code.
+        """
+    PROMPT = PromptTemplate.from_template(TEMPLATE)
+
+    def __init__(self, name, desc="", *examples: Example):
+        super().__init__(name, desc)
+        self.examples = list(examples)
+
+    def add_example(self, input: dict, output):
+        example = Example(input, output)
+        self.examples.append(example)
+        return self
+
+    def to_dict(self):
+        d = super().to_dict()
+        d["examples"] = copy.deepcopy(self.examples)
+        return d
+    
+    def __repr__(self):
+        return f"Name: {self.name}, Description: {self.desc}, Examples: {self.examples}\n"
+    
+
+class ClassSpec(Spec):
+    TEMPLATE = """
+            You are a Python programmer. Your task is to write a simple Python class that matches the following description and has the following methods.
+
+            Description:
+            {desc}
+
+            Instance Methods:
+            {instance_methods}
+
+            Class Methods:
+            {class_methods}
+
+            Static Methods:
+            {static_methods}
 
 
-_add_spec = FuncSpec("add","add two integers (a and b)") \
-                    .add_example({'a':1,'b':2}, "3") \
-                    .add_example({'a':3,'b':4}, "7") \
-                    .add_example({'a':-1,'b':7}, "6") \
-
-print("_add_spec PROMPT",_add_spec.prompt())
-_add_str = _add_spec.invoke(llm)
-print("_add_str",_add_str)
-
-add = _add_spec.parse(_add_str)
-print(add(1,2))
-exit()
-
-_MyClass_spec = ClassSpec("MyClass","a simple class having the methods add")
-_MyClass_spec.add_instance_method("add","add two integers (a and b)",
-            Example({'a':1,'b':2}, "3"),
-            Example({'a':3,'b':4}, "7"),
-            Example({'a':-1,'b':7}, "6"))
-_MyClass_spec.add_class_method("sub","add two integers (a and b)", 
-            Example({'a':1,'b':2}, "3"),
-            Example({'a':3,'b':4}, "7"),
-            Example({'a':-1,'b':7}, "6"),)
-_MyClass_spec.add_static_method("mul","add two integers (a and b)", 
-            Example({'a':1,'b':2}, "2"),
-            Example({'a':3,'b':4}, "12"),
-            Example({'a':-1,'b':7}, "-7"),)
-_MyClass_spec.add_instance_method("div","to divide two integers (a and b)",   
-            Example({'a':1,'b':2}, "0.5"),
-            Example({'a':3,'b':4}, "0.75"),
-            )
-print(_MyClass_spec.prompt())
-MyClassStr = _MyClass_spec.invoke(llm)
-print(MyClassStr)
-
-MyClass = _MyClass_spec.parse(MyClassStr)
-
-print(MyClass)
-myClass = MyClass()
-
-print(myClass.add(1,2))
-
-# f2 = fgen.generate("fibo","fibo sequence (n)",
-#                    Example({'n':1}, "1"),
-#                    Example({'n':2}, "1"),
-#                    Example({'n':3}, "2"),
-#                    Example({'n':4}, "3"),
-#                    Example({'n':5}, "5"),
-#                    Example({'n':6}, "8"),)
+            Write a Python class named `{name}` that implements this logic. Do not include any additional explanation or comments, just the code.
+        """
+    PROMPT = PromptTemplate.from_template(TEMPLATE)
 
 
+    def __init__(self, name, desc=""):
+        super().__init__(name, desc)
+        self.instance_methods = []
+        self.class_methods = []
+        self.static_methods = []
 
+    def add_instance_method(self, name, desc="", *examples: Example):
+        method = FuncSpec(name, desc, *examples)
+        self.instance_methods.append(method)
+        return self
+    
+    def add_class_method(self, name, desc="", *examples: Example):
+        method = FuncSpec(name, desc, *examples)
+        self.class_methods.append(method)
+        return self
+    
+    def add_static_method(self, name, desc="", *examples: Example):
+        method = FuncSpec(name, desc, *examples)
+        self.static_methods.append(method)
+        return self
 
-# #
+    def to_dict(self):
+        d = super().to_dict()
+        d["instance_methods"] = copy.deepcopy(self.instance_methods)
+        d["class_methods"] = copy.deepcopy(self.class_methods)
+        d["static_methods"] = copy.deepcopy(self.static_methods)
+        return d
